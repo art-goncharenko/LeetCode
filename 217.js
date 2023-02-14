2c2d85884f41bb56ba84e7773c732464dacc2a86
Artem Goncharenko <goncharenko.art@gmail.com>
2023-02-13 22:40:35 -0500

    another solution

--- 217.js
+++ 217.js
@@ -7,7 +7,28 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-const containsDuplicate = function(nums) {
-    let set = new Set(nums);
-    return set.size !== nums.length;
-};
+
+// Solution 1: Set
+
+// const containsDuplicate = function(nums) {
+//     let set = new Set(nums);
+//     return set.size !== nums.length;
+// };
+
+// Solution 2: Brute Force
+const containsDuplicate = (nums) => {
+    nums.sort((a, b) => a - b);/* Time O(N * log(N)) | Space O(1 || log(N)) */
+    return hasDuplicate(nums);
+}
+
+const hasDuplicate = (nums) => {
+    for (let curr = 0; curr < (nums.length - 1); curr++) {/* Time O(N) */
+        const next = (curr + 1);
+
+        const isNextDuplicate = nums[curr] === nums[next];
+        if (isNextDuplicate) return true;
+    }
+    return false;
+}
+
+console.log(containsDuplicate([1, 2, 3, 1]));
